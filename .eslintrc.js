fb42d51cf2fadcca0730087244dfe6b4d5c53ed5
spaghettian94 <timer-b@protonmail.com>
2019-11-03 15:42:31 +0900

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    'google'
+  ],
+  plugins: [
+    'svelte3',
+    "import"
+  ],
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js"]
+      }
+    }
+  },
+  overrides: [
+    {
+      files: ['src/**/*.svelte'],
+      processor: 'svelte3/svelte3',
+      rules: {
+        'import/first': 0,
+        'import/no-duplicates': 0,
+        'import/no-mutable-exports': 0,
+      }
+    }
+  ],
+  rules: {
+    "comma-dangle": "off",
+    "func-names": "off",
+    "max-len": "off",
+    "no-console": "off",
+    "no-debugger": "off",
+    "no-invalid-this": "off",
+    "no-extend-native": "off",
+    "import/order": "error",
+    "import/extensions": ["error", "never", {
+      "json": "always"
+    }],
+  }
+}
